512b75bfd9ea031edb8334cfd632b310f31180cd
lcheguend-byte <lcheguend@miumg.edu.gt>
2026-02-18 21:14:10 -0600

    Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -15,24 +15,24 @@ class Cliente : Persona {
             nit = n;
         }
 
-        // MÉTODOS GET Y SET (Encapsulamiento)
-        // Setters para modificar
+        // metodos 
+        //set (modificar)
         void setNit(string n) { nit = n; }
         void setNombres(string nom) { nombres = nom; }
         void setApellidos(string ape) { apellidos = ape; }
-        void setDireccion(string dir) { dirección = dir; }
+        void setDireccion(string dir) { direcciÃ³n = dir; }
         void setTelefono(int tel) { telefono = tel; }
 
-        // Getters para mostrar
+        // get (mostrar)
         string getNit() { return nit; }
         string getNombres() { return nombres; }
         string getApellidos() { return apellidos; }
-        string getDireccion() { return dirección; }
+        string getDireccion() { return direcciÃ³n; }
         int getTelefono() { return telefono; }
 
-        // Método Mostrar (Polimorfismo)
+        // MÃ©todo Mostrar
         void mostrar() {
             cout << "______________________" << endl;
-            cout << nit << ", " << nombres << ", " << apellidos << ", " << dirección << ", " << telefono << endl;
+            cout << nit << ", " << nombres << ", " << apellidos << ", " << direcciÃ³n << ", " << telefono << endl;
         }
 };
